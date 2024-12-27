f99bd9295dfd94949406898d12ab3f6e98106a36
ramtechnow20 <bvhss20@gmail.com>
2024-12-27 15:26:44 +0530

    React_25

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="overall">
+      <div className="outside_box">
+        <div className="inside_box_1">
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+        </div>
+        <div className="inside_box_2"></div>
+      </div>
+    </div>
+  );
+}
+
